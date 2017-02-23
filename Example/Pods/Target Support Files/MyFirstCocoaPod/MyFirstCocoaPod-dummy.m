2d08d130955d2cdbf3b25d7101953343678deaca
Jongwon Woo <jongwonwoo1@gmail.com>
2017-02-23 10:59:38 +0900

    first commit

---
+++ Example/Pods/Target Support Files/MyFirstCocoaPod/MyFirstCocoaPod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MyFirstCocoaPod : NSObject
+@end
+@implementation PodsDummy_MyFirstCocoaPod
+@end
---
+++ Example/Pods/Target Support Files/Pods-MyFirstCocoaPod_Tests/Pods-MyFirstCocoaPod_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyFirstCocoaPod_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_MyFirstCocoaPod_Tests
+@end
